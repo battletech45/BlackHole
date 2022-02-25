bfdf92636f5afb487c60b2f570d476bd97bc5704
Altay Taneri <taneri862@gmail.com>
2022-02-25 21:27:43 +0300

    Profile page updated

--- lib/ProfilePage.dart
+++ lib/ProfilePage.dart
@@ -11,103 +11,65 @@ class ProfilePage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [Colors.deepOrange, Colors.pinkAccent]
-              )
-            ),
+          Expanded(
             child: Container(
-              width: double.infinity,
-              height: 350.0,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    backgroundImage: NetworkImage("https://www.trendrr.net/wp-content/uploads/2017/06/Deepika-Padukone-1.jpg"),
-                    radius: 50.0,
-                  ),
-                  SizedBox(height: 10.0),
-                  Text('Person', style: TextStyle(fontSize: 22.0, color: Colors.white)),
-                  SizedBox(height: 10.0),
-                  Card(margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
-                  clipBehavior: Clip.antiAlias,
-                  color: Colors.white,
-                  elevation: 5.0,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 22.0),
-                    child: Row(
-                      children: [
-                        Expanded(
-                            child: Column(
-                              children: [
-                                Text('Posts', style: TextStyle(color: Colors.redAccent, fontSize: 22.0, fontWeight: FontWeight.bold)),
-                                SizedBox(height: 5.0),
-                                Text('1200', style: TextStyle(fontSize: 20.0, color: Colors.pinkAccent)),
-                              ],
-                            )),
-                        Expanded(
-                            child: Column(
-                              children: [
-                                Text('Followers', style: TextStyle(color: Colors.redAccent, fontSize: 22.0, fontWeight: FontWeight.bold)),
-                                SizedBox(height: 5.0),
-                                Text('21.2k', style: TextStyle(fontSize: 20.0, color: Colors.pinkAccent))
-                              ]
-                        )),
-                        Expanded(
-                            child: Column(
-                              children: [
-                                Text('Follow', style: TextStyle(color: Colors.redAccent, fontSize: 22.0, fontWeight: FontWeight.bold)),
-                                SizedBox(height: 5.0),
-                                Text('1200', style: TextStyle(fontSize: 20.0, color: Colors.pinkAccent))
-                              ],
-                        )),
-                      ],
-                    ),
-                  ),
-                  )
-                ],
-              ),
-            ),
-          ),
-          Container(
-            child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 16.0),
-                child: Column(mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter, colors: [Colors.brown, Colors.black])),
+              child: Container(
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text('Info', style: TextStyle(color: Colors.redAccent, fontStyle: FontStyle.normal, fontSize: 28.0)),
+                    CircleAvatar(
+                      backgroundImage: NetworkImage("https://www.trendrr.net/wp-content/uploads/2017/06/Deepika-Padukone-1.jpg"),
+                      radius: 50.0,
+                    ),
                     SizedBox(height: 10.0),
-                    Text('INFOINFOINFOINFOINFOINFOINFOINFOINFOINFOINFOINFOINFOINFOINFOINFOINFOINFOINFO', style: TextStyle(fontSize: 22.0, fontStyle: FontStyle.italic, fontWeight: FontWeight.w300, color: Colors.black, letterSpacing: 2.0))
+                    Text('Person', style: TextStyle(fontSize: 22.0, color: Colors.white)),
+                    SizedBox(height: 10.0),
+                    Card(
+                      margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                    clipBehavior: Clip.antiAlias,
+                    color: Colors.white,
+                    elevation: 5.0,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 22.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: Column(
+                                children: [
+                                  Text('Posts', style: TextStyle(color: Colors.redAccent, fontSize: 22.0, fontWeight: FontWeight.bold)),
+                                  SizedBox(height: 5.0),
+                                  Text('1200', style: TextStyle(fontSize: 20.0, color: Colors.pinkAccent)),
+                                ],
+                              )),
+                          Expanded(
+                              child: Column(
+                                children: [
+                                  Text('Followers', style: TextStyle(color: Colors.redAccent, fontSize: 22.0, fontWeight: FontWeight.bold)),
+                                  SizedBox(height: 5.0),
+                                  Text('21.2k', style: TextStyle(fontSize: 20.0, color: Colors.pinkAccent))
+                                ]
+                          )),
+                          Expanded(
+                              child: Column(
+                                children: [
+                                  Text('Follow', style: TextStyle(color: Colors.redAccent, fontSize: 22.0, fontWeight: FontWeight.bold)),
+                                  SizedBox(height: 5.0),
+                                  Text('1200', style: TextStyle(fontSize: 20.0, color: Colors.pinkAccent))
+                                ],
+                          )),
+                        ],
+                      ),
+                    ),
+                    ),
                   ],
                 ),
-            ),
-          ),
-          SizedBox(height: 20.0),
-          Container(
-            width: 300.0,
-            child: ElevatedButton(
-              onPressed: (){},
-              child: Ink(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.centerRight,
-                    end: Alignment.centerLeft,
-                    colors: [Colors.pink, Colors.pinkAccent]
-                  ),
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                child: Container(
-                  constraints: BoxConstraints(maxWidth: 300.0, minHeight: 50.0),
-                  alignment: Alignment.center,
-                  child: Text('Contact me', style: TextStyle(color: Colors.white, fontSize: 26.0, fontWeight: FontWeight.w300)),
-                ),
               ),
             ),
-          )
+          ),
         ],
       ),
     );
