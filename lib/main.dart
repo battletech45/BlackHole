40af8a58a7d87ba13a608ec537521b618aa5539f
battletech45 <taneri862@gmail.com>
2023-02-18 01:57:27 +0300

    Firebase options added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:black_hole/LoginPage.dart';
+import 'package:black_hole/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:slider_button/slider_button.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(MyApp());
 }
 
